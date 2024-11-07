3acd946126dca584662532360e80ea4b1208c1b7
владимир назаров <volottnazarov@mail.ru>
2024-11-07 22:59:26 +0300

    Шаблонизация

---
+++ Seminar_6/data.js
@@ -0,0 +1,35 @@
+﻿const dataProducts = `
+    [
+        {
+            "image": "url(img/item2.png)",
+            "title": "КОСТЮМ",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "13.00"
+        },
+        {
+            "image": "url(img/item3.png)",
+            "title": "СВИТЕР",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "99.00"
+        },
+        {
+            "image": "url(img/item4.png)",
+            "title": "КЕДЫ",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "24.00"
+        },
+        {
+            "image": "url(img/item5.png)",
+            "title": "ПИДЖАК",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "79.00"
+        },
+        {
+            "image": "url(img/item6.png)",
+            "title": "БЛУЗКА",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "12.00"
+        }
+
+    ]
+`
---
+++ Seminar_6/script.js
@@ -0,0 +1,45 @@
+﻿const products = JSON.parse(dataProducts);
+
+const itemWrapEl = document.querySelector('.cards__wrapper');
+
+products.forEach(element => {
+    const itemEl = document.createElement('div');
+    itemEl.classList.add('cards__item');
+
+    const imageEl = document.createElement('div');
+    imageEl.classList.add('item-img');
+    imageEl.style.backgroundImage = element.image;
+
+    const descEl = document.createElement('div');
+    descEl.classList.add('item-description');
+
+    const h4El = document.createElement('h4');
+    h4El.classList.add('item-title');
+    h4El.textContent = element.title;
+    descEl.append(h4El);
+
+    const pEl = document.createElement('p');
+    pEl.classList.add('item-text');
+    pEl.textContent = element.description;
+    descEl.append(pEl);
+
+    const priceEl = document.createElement('p');
+    priceEl.classList.add('item-price');
+    priceEl.textContent = `$${element.price}`;
+    descEl.append(priceEl);
+
+    const hoverEl = document.createElement('div');
+    hoverEl.classList.add('item-img__hover');
+    imageEl.append(hoverEl);
+
+    const buttonEl = document.createElement('button');
+    buttonEl.classList.add('item-img__hover-btn');
+    buttonEl.textContent = 'Add to Cart';
+    hoverEl.append(buttonEl);
+
+    
+    itemEl.append(imageEl, descEl);
+    itemWrapEl.append(itemEl);
+});
+
+
