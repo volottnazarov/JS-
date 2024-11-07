3ae54f6071862a72d381e766cff71b1ba3110541
владимир назаров <volottnazarov@mail.ru>
2024-11-07 14:54:41 +0300

    Работа с JSON

---
+++ HomeWork_5/script.js
@@ -0,0 +1,41 @@
+﻿const data = JSON.parse(dataJSON);
+const contentEl = document.getElementById('product-list');
+
+
+data.forEach(elem => {
+    const liEl = document.createElement('li');
+    liEl.style.border = '2px solid gray';
+    liEl.style.backgroundColor = '#F7D7B0';
+    liEl.style.width = '300px';
+
+    const imageEl = document.createElement('img');
+    imageEl.classList.add('content__image');
+    imageEl.src = elem.image;
+    imageEl.style.width = '100px';
+
+    const h5El = document.createElement('h5');
+    h5El.classList.add('content__title');
+    h5El.textContent = elem.title;
+    h5El.style.fontSize = '20px';
+    h5El.style.color = 'blue';
+
+    const pEl = document.createElement('p');
+    pEl.classList.add('content__desc');
+    pEl.textContent = elem.description;
+    pEl.style.fontSize = '14px';
+
+    const priceEl = document.createElement('p');
+    priceEl.classList.add('content__price');
+    priceEl.textContent = ` ${elem.price} USD `;
+    priceEl.style.fontSize = '20px';
+    priceEl.style.color = 'red';
+
+    const buttonEl = document.createElement('button');
+    buttonEl.classList.add('content__button');
+    buttonEl.textContent = 'Add to Cart'
+    buttonEl.style.color = 'yellow';
+    buttonEl.style.backgroundColor = 'green';
+
+    liEl.append(imageEl, h5El, pEl, priceEl, buttonEl);
+    contentEl.append(liEl);
+});
---
+++ Seminar_5/data.js
@@ -0,0 +1,37 @@
+﻿const dataInfo = `
+    [
+        {
+            "number": "2184",
+            "language": "en",
+            "insult": "You are such outrageous, shameless blockheads.",
+            "created": "2024-11-06 21:56:36",
+            "shown": "22031",
+            "createdBy": "Martin Luther",
+            "link": "https://oll-resources.s3.us-east-2.amazonaws.com/oll3/store/people/3858/luther-martin.png",
+            "active": "1",
+            "comment": "https://github.com/stechstudio/insult-service/"
+        },
+        {
+            "number": "1999",
+            "language": "en",
+            "insult": "O you wolf in Christendom!",
+            "created": "2024-11-06 22:17:54",
+            "shown": "22420",
+            "createdBy": "Martin Luther",
+            "link": "https://oll-resources.s3.us-east-2.amazonaws.com/oll3/store/people/3858/luther-martin.png",
+            "active": "1",
+            "comment": "https://github.com/stechstudio/insult-service/"
+        },
+        {
+            "number": "1994",
+            "language": "en",
+            "insult": "What devilish unchristian thing would you not undertake?",
+            "created": "2024-11-06 22:17:21",
+            "shown": "22428",
+            "createdBy": "Martin Luther",
+            "link": "https://oll-resources.s3.us-east-2.amazonaws.com/oll3/store/people/3858/luther-martin.png",
+            "active": "1",
+            "comment": "https://github.com/stechstudio/insult-service/"
+        }
+    ]
+`
